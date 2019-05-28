13c927b4a36f88addbede05f6cabd7bc0b26dd83
agomezvidal <aureli.gomez@gmail.com>
2019-05-28 17:46:58 +0200

    Update listS3.sh

--- listS3.sh
+++ listS3.sh
@@ -2,18 +2,19 @@
 
 LIST="accounts.txt"
 
-echo "start processing account $LIST"
+echo "start processing account $LIST file"
 
 while IFS= read -r line
 do
   #echo "aws s3 ls --profile $line  > ${line}S3list.txt"
   aws s3 ls --profile $line > ${line}S3list.txt
-  if [ -s ${line}S3list.txt ] 
+  if [ -s ${line}S3list.txt ]
     then
       #echo "./subS3.sh ${line}S3list.txt"
       ./subS3.sh ${line}S3list.txt > ${line}_subS3.output 2>&1
     else
-      echo "${line}S3list.txt empty"
+      echo "${line}S3list.txt has no S3 buckets"
    fi
-cat ${line}_subS3.output | grep -B3 "error"
+cat ${line}_subS3.output | grep -B2 "TagSet does not exist"
 done < "$LIST"
+exit $?
